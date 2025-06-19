7e942c3fc12dd083d399320fa62229cb2874a7e2
Krissia Zawadzki <k.dezawadzki@northeastern.edu>
2025-06-19 16:26:13 -0300

    src/qtd: adicionado tipying e espectro do Liouvilliano

--- src/qtd/vectorized_lindbladian_and_davies_map.py
+++ src/qtd/vectorized_lindbladian_and_davies_map.py
@@ -2,6 +2,9 @@
    NOTE: the Davies map is constructed in the eigenbasis of the system Hamiltonian. Transform back to computational basis if needed.
 """
 import numpy as np
+import scipy.linalg
+from typing import Tuple
+
 
 def vectorized_lindbladian(n_sites: int, lind_op_list: list, hamiltonian: np.ndarray) -> np.ndarray:
     """Given a system with n_sites, a hamiltonian and a list of jump operators, returns the vectorized lindbladian
@@ -18,7 +21,7 @@ def vectorized_lindbladian(n_sites: int, lind_op_list: list, hamiltonian: np.nda
     Returns
     -------
     np.ndarray
-        _description_
+        matrix representing the Liouvillian generator in the vectorized form
     """
     vectorized_lindbladian = -1j*np.kron(hamiltonian, np.eye(2**n_sites)) +1j*np.kron(np.eye(2**n_sites), np.transpose(hamiltonian))
     for jump_op in lind_op_list:
@@ -61,7 +64,7 @@ def jump_operators_for_davies_map(n_sites: int, hamiltonian: np.ndarray, beta: f
     list
         list of jump operators that define the Davies dissipator
     """
-    eigw_hamiltonian = np.linalg.eigh(hamiltonian)[0] #FIXME: is this correct?
+    eigw_hamiltonian = np.linalg.eigh(hamiltonian)[0] 
     hamiltonian_diag_basis = np.eye(2**n_sites, dtype=np.complex128)
     lind_op_list = []
     for i in range(2**n_sites):
@@ -106,11 +109,11 @@ def jump_operators_for_davies_map_spin_onehalf(n_sites: int, hamiltonian: np.nda
         list of jump operators that define the Davies dissipator
     """
     import cmath
-    eigw_hamiltonian = np.linalg.eigh(hamiltonian)[0] #FIXME: is this correct?
+    eigw_hamiltonian = np.linalg.eigh(hamiltonian)[0] 
     hamiltonian_diag_basis = np.eye(2**n_sites, dtype=np.complex128)
     
-    lind_op_list = [] #[np.exp(beta/2.* ( eigw_hamiltonian[1] -eigw_hamiltonian[0] ))*np.outer( hamiltonian_diag_basis[:,0], np.conjugate(hamiltonian_diag_basis[:,1]) ),  np.outer( hamiltonian_diag_basis[:,1], np.conjugate(hamiltonian_diag_basis[:,0]) )]
-    
+    lind_op_list = [] 
+
     for i in range(2**n_sites):
         for j in range(i):
             lind_op_list.append( cmath.sqrt( bose_function(beta, eigw_hamiltonian[j]-eigw_hamiltonian[i]) +1 )* np.outer( hamiltonian_diag_basis[:,i], np.conjugate(hamiltonian_diag_basis[:,j]) ) )
@@ -118,3 +121,34 @@ def jump_operators_for_davies_map_spin_onehalf(n_sites: int, hamiltonian: np.nda
 
     return lind_op_list
 
+
+
+def spectrum_of_vectorized_generator(
+    lindbladian_vectorized: np.ndarray
+) -> Tuple[np.ndarray, np.ndarray, np.ndarray, np.ndarray]:
+    """
+    Computes the spectrum (eigenvalues and eigenvectors) of a vectorized
+    generator (e.g., a Lindbladian superoperator).
+
+    The eigenvalues are sorted by the absolute value of their real parts.
+
+    Parameters:
+        lindbladian_vectorized (np.ndarray): Square matrix representing the 
+                                              vectorized generator.
+
+    Returns:
+        Tuple containing:
+            - spectrum (np.ndarray): Array of shape (N, 2) with real and imaginary 
+                                     parts of eigenvalues. Columns: [Re(λ), Im(λ)].
+            - eigw_vectorized_lindbladian (np.array): Array of shape (N) containing also the ordered eigenvalues.
+            - left_eigenvectors (np.ndarray): Matrix whose columns are left eigenvectors.
+            - right_eigenvectors (np.ndarray): Matrix whose columns are right eigenvectors.
+    """
+    eigw_vectorized_lindbladian, left_eigv_vectorized_lindbladian, right_eigv_vectorized_lindbladian = scipy.linalg.eig(lindbladian_vectorized, left=True, right=True)
+    idx = np.argsort(np.abs(np.real(eigw_vectorized_lindbladian)))
+    eigw_vectorized_lindbladian = eigw_vectorized_lindbladian[idx]
+    left_eigv_vectorized_lindbladian = left_eigv_vectorized_lindbladian[:,idx]
+    right_eigv_vectorized_lindbladian = right_eigv_vectorized_lindbladian[:,idx]
+    spectrum = np.column_stack((np.real(eigw_vectorized_lindbladian), np.imag(eigw_vectorized_lindbladian))) 
+    
+    return spectrum, eigw_vectorized_lindbladian, left_eigv_vectorized_lindbladian, right_eigv_vectorized_lindbladian
